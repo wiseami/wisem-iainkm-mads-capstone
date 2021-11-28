549ef5af25fd787551441c4a356fed10a031f320
Iain KM <iainkm@umich.edu>
2021-11-28 12:44:28 -0500

    Cleaned up MIR feature data types and updated full feature csv. Added new hierarchical clustering ipynb

--- Utility Functions.py
+++ Utility Functions.py
@@ -20,11 +20,26 @@ def combine_csv(file_1, file_2, column, new_file):
             d.rename(columns={'id':'track_id'}, inplace=True)
     df_f = df_1.merge(df_2, how='left', on=[column])
     df_f.to_csv(new_file)
+    print('Files combined.')
 
 
+def conv_type(file):
+    df = pd.read_csv(file)
+    col_name = ['chroma', 'chroma_cens', 'mff', 'spectral_centroid',
+                'spectral_bandwidth', 'spectral_contrast', 'spectral_flatness',
+                'Spectral_Rolloff', 'poly_features', 'tonnetz', 'ZCR', 'onset_strength',
+                'pitch', 'magnitude', 'tempo']
+    for col in col_name:
+        df[col] = [i.strip('array()[]') for i in df[col]]
+        df[col] = pd.to_numeric(df[col])
+    df.to_csv(file)
+    print('Columns converted to float type.')
 
-combine_csv('lookups/track_audio_features.csv', 'lookups/MIR_features.csv', 'track_id', 'lookups/full_audio_features')
 
-# dedupe('lookups/MIR_features.csv', 'track_id')
+dedupe('lookups/MIR_features.csv', 'track_id')
+conv_type('lookups/MIR_features.csv')
+combine_csv('lookups/track_audio_features.csv', 'lookups/MIR_features.csv', 'track_id', 'lookups/full_audio_features.csv')
+
+
 
 
