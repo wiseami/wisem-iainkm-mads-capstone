34bfe84f63c743c2ff8f75b43f402073990bea10
wiseami <42419916+wiseami@users.noreply.github.com>
2021-12-12 13:27:38 -0500

    moved daily files into own folder removed extraneous comments updated readme

--- README.md
+++ README.md
@@ -1,27 +1,24 @@
-# wisem-iainkm-mads-capstone
+# Music Affinity Across Geographical Boundaries
+Capstone project for University of Michigan's Master of Applied Data Science program by Mike Wise and Iain King-Moore utilizing data pulled using Spotify's API to understand audio feature sameness across the world.
 
-
-
-
-
-
-
-# Recommender Quick Start
+# Recommendation Engine Quick Start
 Beyond the data analysis aspect of this repository, one of the key features is a music recommendation engine utilizing Spotify's API. Once launched, you're able to search for any artist, pick one of their top 5 songs and get back a recommended Daily Top 50 playlists from somewhere in the world that likely aligns with your choice.
 
 ## Spotify API Setup
 
 Since portions of this code require a tie-in to Spotify's API, you'll need to create an account and register a basic app with them.
-More information [here.](https://developer.spotify.com/documentation/general/guides/authorization/app-settings/)
-Make sure when registering your app, you note down your CLIENT_ID and CLIENT_SECRET.
+[Spotify](https://developer.spotify.com/documentation/general/guides/authorization/app-settings/) has a great step-by-step guide on creating a simple app needed to leverage their API.
+\
+Make sure when registering your app, you note down your CLIENT_ID and CLIENT_SECRET as you'll need those for a Streamlit configuration file.
 
 ## Streamlit
 ### Installation
+In order for you to sucessfully clone this repository and run the Streamlit app locally, you'll need the Streamlit library.
 ```
 pip install streamlit
 ```
 
-More complete instructions here at the [Streamlit repository.](https://github.com/streamlit/streamlit)
+For more information about Streamlit in general, check out their [repository.](https://github.com/streamlit/streamlit)
 
 ### Plugging in Spotify Information
 Remember the CLIENT_ID and CLIENT_SECRET you were supposed to record? In order to utilize some of the live API functionality of the Streamlit app, plug those values into the ```secrets.toml``` file in the ```/.streamlit``` directory.
@@ -39,5 +36,5 @@ streamlit run streamlit_app.py
 
 
 
-
+Check out our already published app hosted in Streamlit Cloud!\
 [![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://share.streamlit.io/wiseami/wisem-iainkm-mads-capstone/main/)
--- playlist_scrape.py
+++ playlist_scrape.py
@@ -9,8 +9,8 @@ import utils
 headers, market, BASE_URL = utils.spotify_info()
 
 # Read in our csv lookup with all 69 Daily Song Charts
-file_path = os.path.dirname(os.path.abspath(__file__)) + '\\'
-playlist_lookup = pd.read_csv(file_path + 'lookups\\global_top_daily_playlists.csv')
+file_path = os.path.dirname(os.path.abspath(__file__)) + '/'
+playlist_lookup = pd.read_csv(file_path + 'lookups/global_top_daily_playlists.csv')
 
 # Get the most recent updated time stamp from the first song in the Global playlist
 # This tells us if the playlists were updated yet today or not since it doesn't seem to be any sort of set schedule on the Spotify side
@@ -21,7 +21,7 @@ for tracks in playlist_tracks['items'][:1]:
     new_updated_date = pd.to_datetime(tracks['added_at']).date()
 
 # Get the last updated time stamp from master file
-playlist_scrape_lookup = pd.read_csv(file_path + 'playlist_data\\playlist_data.csv')
+playlist_scrape_lookup = pd.read_csv(file_path + 'playlist_data/playlist_data.csv')
 last_updated_date = pd.to_datetime(playlist_scrape_lookup['track_added_date']).dt.date.max()
 
 # Check the new updated time stamp against the max value in our existing playlist data
@@ -42,7 +42,6 @@ if new_updated_date > last_updated_date:
             track_list.append(tracks['track']['artists'][0]['name'])
             track_list.append(tracks['track']['name'])
             track_list.append(tracks['track']['id'])
-            # track_release_date = tracks['track']['release_date'] ## wtf does this not work?
             track_list.append(tracks['track']['popularity'])
             track_list.append(track_num)
             track_list.append(tracks['track']['preview_url'])
@@ -54,8 +53,8 @@ if new_updated_date > last_updated_date:
     track_df = pd.DataFrame(final_list, columns=(['country','market','capture_dttm','track_artist','track_name','track_id','track_popularity','track_number','track_preview_url','track_duration','track_added_date']))
 
     f_name = str(track_df['capture_dttm'][0].date())
-    track_df.to_csv(file_path + 'playlist_data\\' + f_name + '.csv', index=False)
-    track_df.to_csv(file_path + 'playlist_data\\playlist_data.csv', mode='a',header=False, index=False)    
+    track_df.to_csv(file_path + 'playlist_data/daily_data/' + f_name + '.csv', index=False)
+    track_df.to_csv(file_path + 'playlist_data/playlist_data.csv', mode='a',header=False, index=False)    
 
     print('New playlist data loaded')
     #exit(0)
